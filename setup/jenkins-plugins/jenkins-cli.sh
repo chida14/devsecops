53b83baa35f651f07d96b553834351dd23d2d83c
chida <chidanandmm@gmail.com>
2025-12-18 18:53:46 +1100

    added new plugin download scrip

---
+++ setup/jenkins-plugins/jenkins-cli.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eo pipefail
+
+JENKINS_URL="http://devsecops-chida.australiaeast.cloudapp.azure.com:8080/"
+AUTH="admin:${JENKINS_TOKEN}"   # or admin:YOUR_API_TOKEN
+
+java -jar jenkins-cli.jar -s "$JENKINS_URL" -auth "$AUTH" install-plugin \
+  performance docker-workflow dependency-check-jenkins-plugin blueocean \
+  coverage \
+  slack sonar pitmutation kubernetes-cli \
+  -deploy -restart
+
